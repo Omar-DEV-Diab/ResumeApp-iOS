4f8e22ab90f762935291f6520035653a6027ea77
Omar Diab <omar_diab_1995@yahoo.com>
2022-02-22 18:06:06 +0200

    Initial Commit

---
+++ ResumeApp-iOS-AnyMind-Group/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ResumeApp-iOS-AnyMind-Group
+//
+//  Created by Omar Diab on 2/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
